67873608e3d31455925ea120eeff32988ef0d5c2
ayami.toide <ayami.toide@rakuten.com>
2018-08-23 14:58:57 +0900

    Initial commit

---
+++ src/main/java/com/example/sixth_sense/SixthSenseApplication.java
@@ -0,0 +1,12 @@
+package com.example.sixth_sense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SixthSenseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SixthSenseApplication.class, args);
+    }
+}
