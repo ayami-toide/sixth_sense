6ec575e9359225144d0ed28297694158d2422589
yuki.osanai <kksp5112@gmail.com>
2018-08-29 14:52:47 +0900

    'a'

--- src/main/java/com/example/sixth_sense/controller/WordListController.java
+++ src/main/java/com/example/sixth_sense/controller/WordListController.java
@@ -1,13 +1,10 @@
 package com.example.sixth_sense.controller;
 
 
-import com.example.sixth_sense.service.WordListService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -17,13 +14,31 @@ import java.util.List;
 @RequestMapping("/sense")
 public class WordListController {
 
-    @Autowired
-    private WordListService wordlistService;
-
     @GetMapping("test")
     public String test(Model model){
         return "test";
     }
+
+    @GetMapping("mypage")
+    public String mypage(Model model){
+        return "mypage";
+    }
+
+    @GetMapping("study")
+    public String study(Model model){
+        return "study";
+    }
+
+    @GetMapping("review")
+    public String review(Model model){
+        return "review";
+    }
+
+    @GetMapping("test_result")
+    public String test_result(Model model) {
+        return "test_result";
+    }
+
 
     @RequestMapping("test_quiz")
     public String test_quiz(Model model){
@@ -36,7 +51,6 @@ public class WordListController {
         Collections.shuffle(list);
         String[] words =list.toArray(new String[0]);
 
-
         String m1 = list.get(0);
         String m2= list.get(1);
         String m3 = list.get(2);
@@ -47,17 +61,9 @@ public class WordListController {
         model.addAttribute("m2", m2);
         model.addAttribute("m3", m3);
         model.addAttribute("m4", m4);
-        model.addAttribute("words",wordlistService.findAll());
+        model.addAttribute("words",words);
         return "test_quiz";
 
-
-    }
-
-    @RequestMapping("test_result")
-    public String test_result(Model model)
-    {
-
-        return "test_result";
     }
 
 
