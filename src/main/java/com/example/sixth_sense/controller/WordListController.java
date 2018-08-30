ecf9008858278c5da353ee76591a570e4b8d939f
ayami.toide <ayami.toide@rakuten.com>
2018-08-30 13:26:19 +0900

    'aa'

--- src/main/java/com/example/sixth_sense/controller/WordListController.java
+++ src/main/java/com/example/sixth_sense/controller/WordListController.java
@@ -28,30 +28,21 @@ public class WordListController {
     @RequestMapping("test_quiz")
     public String test_quiz(Model model){
 
-
-
         String[] array = {"信じる", "嫌い", "遊ぶ", "かわいい", "TRUST"};
         List<String> list = Arrays.asList(array);
         String word = list.get(4);
         Collections.shuffle(list);
         String[] words =list.toArray(new String[0]);
-
-        String m1 = list.get(0);
-        String m2= list.get(1);
-        String m3 = list.get(2);
-        String m4 = list.get(3);
-
-        model.addAttribute("word", word);
-        model.addAttribute("m1", m1);
-        model.addAttribute("m2", m2);
-        model.addAttribute("m3", m3);
-        model.addAttribute("m4", m4);
+        int id = 1;
+        model.addAttribute("id", id);
         model.addAttribute("words",wordlistService.findOne(new Long(1)));
 
         return "test_quiz";
 
 
     }
+
+
 
     @RequestMapping("test_result")
     public String test_result(Model model)
