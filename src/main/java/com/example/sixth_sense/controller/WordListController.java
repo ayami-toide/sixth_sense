b745ca17fdd01a1693b5eb6cf30083dd572b49e8
ayami.toide <ayami.toide@rakuten.com>
2018-09-10 14:48:41 +0900

    'progressbarcolor'

--- src/main/java/com/example/sixth_sense/controller/WordListController.java
+++ src/main/java/com/example/sixth_sense/controller/WordListController.java
@@ -111,9 +111,19 @@ public class WordListController {
         }
 
 
+        String[] array = {"今日もおつかれ", "Excellent", "never give up", "君ならできる"};
+        List<String> list = Arrays.asList(array);
+        Collections.shuffle(list);
+        String word  = list.get(1);
+        array = (String[])list.toArray(new String[0]);
+        model.addAttribute("word",word);
+
+
         model.addAttribute("words",wordlistService.findOne(new Long(1)));
         return "test_result";
     }
+
+
 
 
 
--- src/main/resources/static/js/flicker.js
+++ src/main/resources/static/js/flicker.js
@@ -35,3 +35,5 @@ $(document).ready(function(){
 
 
 //study.html ここまで//
+
+
