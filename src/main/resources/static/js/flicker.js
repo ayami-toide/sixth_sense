4f3856dbe63d23eb93e974130a348450fbcb9b08
Unemura <rina.unemura@rakuten.com>
2018-09-11 10:00:33 +0900

    'a'

--- src/main/resources/static/js/flicker.js
+++ src/main/resources/static/js/flicker.js
@@ -7,7 +7,7 @@ $(function() {
 
 $(document).ready(function(){
 
-    $(".buddy").on("swiperight",function(){
+    $(".flick1").on("swiperight",function(){
         $(this).addClass("rotate-left").delay(700).fadeOut(1);
         $(".buddy").find(".status").remove();
 
@@ -17,23 +17,93 @@ $(document).ready(function(){
                 $('.rotate-right').removeClass('rotate-right').fadeIn(300);
             }, 1000);
         }
-
-        // var data = {word: document.getElementById("flick_word").textContent};
-        //
-        // $.ajax ({
-        //     url: "/sense/study1",
-        //     type: "POST",
-        //     data : JSON.stringify(data),
-        //     contentType: 'application/JSON',
-        //     dataType : 'JSON',
-        //     success: function() {
-        //         console.log("success");
-        //     }
-        // });
-
     });
 
-    $(".buddy").on("swipeleft",function(){
+    $(".flick1").on("swipeleft",function(){
+        $(this).addClass('rotate-right').delay(700).fadeOut(1);
+        $('.buddy').find('.status').remove();
+        $(this).append('<div class="status dislike">知らない!</div>');
+
+        $('.rotate-left').remove();
+
+        if ( $(this).is(':first-child') ) {
+            $('.rotate-right').removeClass('rotate-right').fadeIn(300);
+            setTimeout(function () {
+                $('.buddy').find('.status').remove();
+                $('.rotate-right').removeClass('rotate-right').fadeIn(300);
+            }, 900);
+        }
+    });
+
+    $(".flick2").on("swiperight",function(){
+        $(this).addClass("rotate-left").delay(700).fadeOut(1);
+        $(".buddy").find(".status").remove();
+
+        $(this).append('<div class="status like">Like!</div>');
+        if ( $(this).is(':first-child') ) {
+            setTimeout(function () {
+                $('.rotate-right').removeClass('rotate-right').fadeIn(300);
+            }, 1000);
+        }
+    });
+
+    $(".flick2").on("swipeleft",function(){
+        $(this).addClass('rotate-right').delay(700).fadeOut(1);
+        $('.buddy').find('.status').remove();
+        $(this).append('<div class="status dislike">知らない!</div>');
+
+        $('.rotate-left').remove();
+
+        if ( $(this).is(':first-child') ) {
+            $('.rotate-right').removeClass('rotate-right').fadeIn(300);
+            setTimeout(function () {
+                $('.buddy').find('.status').remove();
+                $('.rotate-right').removeClass('rotate-right').fadeIn(300);
+            }, 900);
+        }
+    });
+
+    $(".flick3").on("swiperight",function(){
+        $(this).addClass("rotate-left").delay(700).fadeOut(1);
+        $(".buddy").find(".status").remove();
+
+        $(this).append('<div class="status like">Like!</div>');
+        if ( $(this).is(':first-child') ) {
+            setTimeout(function () {
+                $('.rotate-right').removeClass('rotate-right').fadeIn(300);
+            }, 1000);
+        }
+    });
+
+    $(".flick3").on("swipeleft",function(){
+        $(this).addClass('rotate-right').delay(700).fadeOut(1);
+        $('.buddy').find('.status').remove();
+        $(this).append('<div class="status dislike">知らない!</div>');
+
+        $('.rotate-left').remove();
+
+        if ( $(this).is(':first-child') ) {
+            $('.rotate-right').removeClass('rotate-right').fadeIn(300);
+            setTimeout(function () {
+                $('.buddy').find('.status').remove();
+                $('.rotate-right').removeClass('rotate-right').fadeIn(300);
+            }, 900);
+        }
+    });
+
+    $(".flick4").on("swiperight",function(){
+        $(this).addClass("rotate-left").delay(700).fadeOut(1);
+        $(".buddy").find(".status").remove();
+
+        $(this).append('<div class="status like">Like!</div>');
+        if ( $(this).is(':first-child') ) {
+            setTimeout(function () {
+                $('.rotate-right').removeClass('rotate-right').fadeIn(300);
+            }, 1000);
+        }
+    });
+
+    $(".flick4").on("swipeleft",function(){
         $(this).addClass('rotate-right').delay(700).fadeOut(1);
         $('.buddy').find('.status').remove();
         $(this).append('<div class="status dislike">知らない!</div>');
