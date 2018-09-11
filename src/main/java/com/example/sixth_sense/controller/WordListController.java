83b81e8a51bb5ae843eca252285555d94f793d68
Fujimoto <kazane.fujimoto@rakuten.com>
2018-09-11 15:18:24 +0900

    'sss'

--- src/main/java/com/example/sixth_sense/controller/WordListController.java
+++ src/main/java/com/example/sixth_sense/controller/WordListController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -37,9 +38,10 @@ public class WordListController {
 
     @GetMapping("study")
     public String study(Model model){
-//    model.addAttribute("id", id);
 
-        model.addAttribute("words",wordlistService.findWords());
+        model.addAttribute("newWords1", wordlistService.findNewWords1());
+        model.addAttribute("newWords2", wordlistService.findNewWords2());
+
         return "study";
     }
 
@@ -82,24 +84,24 @@ public class WordListController {
     @RequestMapping("test_result/{id}")
     public String test_result(@PathVariable("id") int id,Model model)
     {
-              int resultId1 = id - 5;
-              model.addAttribute("testwords1",wordlistService.findTestWord(new Long(resultId1)));
-              int resultId2 = id - 4;
-              model.addAttribute("testwords2",wordlistService.findTestWord(new Long(resultId2)));
-              int resultId3 = id - 3;
-              model.addAttribute("testwords3",wordlistService.findTestWord(new Long(resultId3)));
-              int resultId4 = id - 2;
-              model.addAttribute("testwords4",wordlistService.findTestWord(new Long(resultId4)));
-              int resultId5 = id - 1;
-              model.addAttribute("testwords5",wordlistService.findTestWord(new Long(resultId5)));
+        int resultId1 = id - 5;
+        model.addAttribute("testwords1",wordlistService.findTestWord(new Long(resultId1)));
+        int resultId2 = id - 4;
+        model.addAttribute("testwords2",wordlistService.findTestWord(new Long(resultId2)));
+        int resultId3 = id - 3;
+        model.addAttribute("testwords3",wordlistService.findTestWord(new Long(resultId3)));
+        int resultId4 = id - 2;
+        model.addAttribute("testwords4",wordlistService.findTestWord(new Long(resultId4)));
+        int resultId5 = id - 1;
+        model.addAttribute("testwords5",wordlistService.findTestWord(new Long(resultId5)));
 
-              model.addAttribute("testmeanings1",wordlistService.findTestMeaning(new Long(resultId1)));
-              model.addAttribute("testmeanings2",wordlistService.findTestMeaning(new Long(resultId2)));
-              model.addAttribute("testmeanings3",wordlistService.findTestMeaning(new Long(resultId3)));
-              model.addAttribute("testmeanings4",wordlistService.findTestMeaning(new Long(resultId4)));
-              model.addAttribute("testmeanings5",wordlistService.findTestMeaning(new Long(resultId5)));
+        model.addAttribute("testmeanings1",wordlistService.findTestMeaning(new Long(resultId1)));
+        model.addAttribute("testmeanings2",wordlistService.findTestMeaning(new Long(resultId2)));
+        model.addAttribute("testmeanings3",wordlistService.findTestMeaning(new Long(resultId3)));
+        model.addAttribute("testmeanings4",wordlistService.findTestMeaning(new Long(resultId4)));
+        model.addAttribute("testmeanings5",wordlistService.findTestMeaning(new Long(resultId5)));
 
-              model.addAttribute("id", id);
+        model.addAttribute("id", id);
 
         String[] array = {"今日もおつかれ", "Excellent", "never give up", "君ならできる"};
         List<String> list = Arrays.asList(array);
@@ -109,9 +111,9 @@ public class WordListController {
         model.addAttribute("word",word);
 
 
-   
-        return "test_result";
+        model.addAttribute("words",wordlistService.findOne(new Long(1)));
 
+        return "test_result";
     }
 
     @GetMapping("unit_select")
@@ -119,8 +121,19 @@ public class WordListController {
         return "unit_select";
     }
 
+    @GetMapping("teacher_unit_select")
+    public String teacher_unit_select(Model model){
+
+        String teacherId = "teacherId";
+        model.addAttribute("teacherId",teacherId);
+
+        return "unit_select";
+    }
+
     @GetMapping("teacher_page")
     public String teacher_page(Model model){
+
+
         return "teacher_page";
     }
 
@@ -135,4 +148,3 @@ public class WordListController {
 
 
 }
-
